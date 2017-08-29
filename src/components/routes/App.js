dc5986b70c65fe2249ab75bd163504552a52d91e
David Linke <linke@davelinke.com>
2017-08-29 15:51:30 -0400

    Added google font similar to the brand i am targeting. created base CSS added timer logic to the app component added helper classes to aid on the fullscreen layout created reducers for post activation sat default values for posts.

--- src/components/routes/App.js
+++ src/components/routes/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import store from '../../store';
 import * as lang from '../../static/languages';
 import * as api from '../api/interface';
 import Post from '../posts/Post';
@@ -10,7 +11,25 @@ import './App.css';
 class App extends Component {
     constructor(props){
         super(props);
+        // get the data from the API
         api.getData();
+    }
+    componentDidMount(){
+        // let's rotate them posts
+        var intervalId = setInterval(this.timer, this.props.ui.intervalLength);
+        // despite the use of redux to control state, this is an instance where the calue won't be used anywhere else,
+        // so we use component state
+        this.setState({intervalId: intervalId});
+    }
+    componentWillUnmount() {
+       // clean the mess
+       clearInterval(this.state.intervalId);
+    }
+    timer(){
+        //dispatch the active element
+        store.dispatch({
+            type:'SOCIAL_NEXT_POST'
+        })
     }
     renderPosts(posts){
         return posts.map((post,i)=>{
@@ -19,9 +38,9 @@ class App extends Component {
     }
     render() {
         return (
-            <div className={this.props.ui.appClassName}>
-                <h1 className="intro"><span className={this.props.ui.showHeadingText?'':'sr-only'}>{lang.write('main_title')}</span></h1>
-                <div>{this.renderPosts(this.props.socialData.posts)}</div>
+            <div className={'full-screen '+this.props.ui.appClassName}>
+                <h1 className="intro full-screen"><span className={this.props.ui.showHeadingText?'':'sr-only'}>{lang.write('main_title')}</span></h1>
+                <div className="posts full-screen">{this.renderPosts(this.props.socialData.posts)}</div>
             </div>
         );
     }
